401d694b8ee9c702e0a8c020b7bc3d62ec13d7d4
Shota Kudo <in-the-cloud.950@hotmail.co.jp>
2025-05-29 22:24:15 +0900

    p.172まで

--- workshop/chapter03/function.clj
+++ workshop/chapter03/function.clj
@@ -22,3 +22,33 @@
                    (count friends)
                    " friend request(s) to the following players: "
                    (clojure.string/join ", " friends))))))
+
+;; First-Class Functions
+(update {:item "Tomato" :price 1.0} :price (fn [x] (/ x 2)))
+(update {:item "Tomato" :price 1.0} :price / 2)
+(update {:item "Tomato" :fruit false} :fruit not)
+
+(defn operate [f x] (f x))
+(operate inc 5) ;;=> 6
+
+(defn operate [f & args] (f args))
+(operate + 1 2 3)
+;; Execution error (ClassCastException) at java.lang.Class/cast (Class.java:4067).
+;; Cannot cast clojure.lang.ArraySeq to java.lang.Number
+
+;; (+ [1 2 3]) のような状況になっている
+
+(apply + [1 2 3]) ;;=> 6
+
+(defn operate [f & args] (apply f args))
+(operate + 1 2 3)
+
+(defn random-fn [] (first (shuffle [+ - * /])))
+(random-fn)
+((random-fn) 2 4) ;;=> -2
+;; 評価されないと
+(fn? random-fn) ;;=> true
+(fn? (random-fn)) ;;=> true
+
+(let [mysterious-fn (random-fn)]
+  (mysterious-fn 3 7))
